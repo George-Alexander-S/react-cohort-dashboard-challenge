6b2a71f3eee7b9427e502159729c662bc6f7ebeb
George <george.alexander.saav@gmail.com>
2024-10-11 15:43:50 +0200

    Still no luck in refactoring PostItems to use the userprovider

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,8 +11,6 @@ function UserProfile() {
     useEffect(() => {
         getUser(userId);
     }, [userId]) 
-    
-    // Sweet, including userId in the dependency array makes sure it updates, even if im aldready in the profile comp in another user, then pressing on the profile icon which should take me to the hardcoded user1. The useEffect will trigger on the new userId, and refresh the component.
 
     if (!user) {
         return <p>Loading...</p>;
